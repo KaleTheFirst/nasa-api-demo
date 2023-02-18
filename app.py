efeea5ee5fca8ecbf205f526174a227ac23cc3d0
KaleTheFirst <kyle.brooks@csedge.org>
2023-02-18 15:56:00 -0500

    Success

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
-from datetime import date
+
 
 app = Flask(__name__)
-api_key = 'v5c19WhzIBKZKwuTc0LHo7fX8TSl81hddkCGyXNz'
+
 
 @app.route('/')
 def index():
-    today = "2006-05-16"
-    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={api_key}&date={today}')
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['GET','POST'])
+def search():
+    if request.method == 'GET':
+        input = request.args['fname']
+    else:
+        input = request.form['fname']
+    
+    response = requests.get(f'https://fruityvice.com/api/fruit/{input}')
     data = response.json()
-    return render_template('index.html' , data = data, date = today)
+    # data = {
+    # "genus": "Citrus",
+    # "name": "Orange",
+    # "id": 2,
+    # "family": "Rutaceae",
+    # "order": "Sapindales",
+    # "nutritions": {
+    #     "carbohydrates": 8.3,
+    #     "protein": 1,
+    #     "fat": 0.2,
+    #     "calories": 43,
+    #     "sugar": 8.2
+    # }
+    # }   
+    return render_template('result.html' , data = data)
+
 
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0')
